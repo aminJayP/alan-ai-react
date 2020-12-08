d78537b2531697441be2dc7f7e262141160a3dca
aminJayP <53985903+aminJayP@users.noreply.github.com>
2020-12-08 18:51:16 -0500

    Fixed linting errors

--- src/App.js
+++ src/App.js
@@ -3,52 +3,48 @@ import alanBtn from '@alan-ai/alan-sdk-web';
 
 import wordsToNumbers from 'words-to-numbers';
 
-import NewsCards from './components/NewsCards/NewsCards'
-import useStyles from './styles'
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles';
 
-
-
-
-const alankey = 'API KEY'
+const alankey = 'API KEY';
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const classes = useStyles();
 
-    const [newsArticles, setNewsArticles] = useState([]);
-    const [activeArticle, setActiveArticle] = useState(-1);
-    const classes = useStyles()
+  useEffect(() => {
+    alanBtn({
+      key: alankey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parseNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+          const article = articles[parseNumber - 1];
 
-    useEffect(() => {
-        alanBtn({
-            key: alankey,
-            onCommand: ( { command, articles, number } ) => {
-                if(command === 'newHeadlines'){
-                  setNewsArticles(articles);
-                  setActiveArticle(-1);
-                } else if(command === 'highlight') {
-                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
-                } else if(command === 'open') {
-                    const parseNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }): number;
-                    const article = articles[parseNumber - 1];
+          if (parseNumber > 20) {
+            alanBtn().playText('Please try that again');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          }
+        }
+      },
+    });
+  }, []);
 
-                    if(parseNumber > 20){
-                        alanBtn().playText('Please try that again')
-                    } else if(article) {
-                        window.open(article.url, '_blank');
-                        alanBtn().playText('Opening...');
-                    }
-                }
-            }
-        })
-    }, [])
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <div className={classes.logoContainer}>
-                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo"/>
-            </div>
-            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
-        </div>
-    )
-}
-
-export default App
+export default App;
